9cd69a8e1040f4d33f4e890144ff9b863fdcfb0e
SebSCam <sebas9903@outlook.com>
2021-07-06 16:58:36 -0500

    final commit

--- Server.js
+++ Server.js
@@ -1,13 +1,22 @@
 const express =  require('express')
 const app = express()
-const bodyParser = require('body-parser');
+const post = require('./post_service.js')
+const get = require('./get_service.js')
+const del = require('./delete_service.js')
 
+app.use(express.urlencoded({ extended: false }));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/post', post)
+app.use('/get', get)
+app.use('/del', del)
 
-app.use('/post', require ('./post_service.js'))
-app.use('/get', require ('./get_service.js'))
-app.use('/delete', require ('./delete_service.js'))
+app.get('/',function(req,res){
+    res.sendFile(__dirname +'/index.html')
+})
+
+app.post('/post', post.post)
+app.get('/get', get.get)
+app.delete('/del', del.delete)
 
 app.listen(3000,()=>{
     console.log('Server running http://localhost:3000')
--- post_service.js
+++ post_service.js
@@ -1,16 +1,17 @@
 const express =  require('express')
 const app = express.Router()
-const bodyParser = require('body-parser');
 const fs = require('fs')
 const file = './datos.json'
 var data = []
 
 data = JSON.parse(fs.readFileSync(file))
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: true}));
 
-app.post('/post',function(req,res){
+app.post('/',function(req,res){
     console.log(req.body.name)
     data.push(req.body.name)
+    let jsonData=JSON.stringify(data)
+    fs.writeFileSync(file, jsonData)
 })
 
 module.exports = app;
